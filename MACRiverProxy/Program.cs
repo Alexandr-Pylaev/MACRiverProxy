63d41ba2d230d2de7b27e16b51f6949c96ad1b1f
FantomLis <71078845+FantomLis@users.noreply.github.com>
2026-01-22 10:44:15 +0300

    Add serilog

--- MACRiverProxy/Program.cs
+++ MACRiverProxy/Program.cs
@@ -1,9 +1,14 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));;
-
+builder.Services.AddSerilog(ser =>
+{
+    ser.WriteTo.Console();
+});
 var app = builder.Build();
 
 app.MapReverseProxy();
-
+app.UseSerilogRequestLogging();
 app.Run();
