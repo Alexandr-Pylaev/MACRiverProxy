f7efd789beaf724d434540c0c1f96edcd087195f
FantomLis <71078845+FantomLis@users.noreply.github.com>
2026-01-22 10:54:49 +0300

    Add warning for disabled HTTPS urls

--- MACRiverProxy/Program.cs
+++ MACRiverProxy/Program.cs
@@ -11,4 +11,29 @@ var app = builder.Build();
 
 app.MapReverseProxy();
 app.UseSerilogRequestLogging();
-app.Run();
+
+app.Start();
+
+bool isHttpsEnabled = false;
+foreach (var url in app.Urls)
+{
+    if (url.ToLower().StartsWith("https://"))
+    {
+        isHttpsEnabled = true;
+        break;
+    }
+}
+
+if (!isHttpsEnabled)
+{
+    Log.Error("=========================================");
+    Log.Error("HTTPS is not enabled on this proxy.");
+    Log.Error("This means, that all proxy traffic is transferred in plain-text."); 
+    Log.Error("Please enable HTTPS for traffic encryption.");
+    Log.Error("=========================================");
+}
+
+while (!app.Lifetime.ApplicationStopping.IsCancellationRequested)
+{
+    
+}
