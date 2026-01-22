819835d3393b2f060b88f62fda0bc2351af009eb
FantomLis <71078845+FantomLis@users.noreply.github.com>
2026-01-22 11:34:03 +0300

    Program reformat

--- MACRiverProxy/Program.cs
+++ MACRiverProxy/Program.cs
@@ -8,52 +8,66 @@ using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
+#region App builder setup
+
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));;
 builder.Services.AddSerilog(ser =>
 {
     ser.WriteTo.Console();
 });
+
+#endregion
+
+#region App builder auth setup
+
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
     {
         options.LoginPath = "/login";
         options.LogoutPath = "/logout";
     });
+
 builder.Services.AddAuthorization(options =>
 {
     options.AddPolicy("none", policyBuilder =>
     {
-        policyBuilder.Requirements.Add(new AssertionRequirement(a => true));
+        policyBuilder.Requirements.Add(new AssertionRequirement(_ => true));
     });
     options.AddPolicy("restricted", policyBuilder =>
     {
         policyBuilder.RequireAuthenticatedUser();
     });
 });
+
+#endregion
+
 var app = builder.Build();
+
+#region App auth setup
 
 app.UseAuthentication();
 app.UseAuthorization();
+
+#endregion
+
+#region Login pages
+
 app.MapGet("/login", (context) => context.Request.HttpContext.
     SignInAsync(new ClaimsPrincipal(new ClaimsIdentity([new Claim("test", "100")], CookieAuthenticationDefaults.AuthenticationScheme))));
 app.MapGet("/logout", (context) => context.Request.HttpContext.SignOutAsync());
-app.MapGet("/hi",  context =>
-{
-    StringBuilder a = new();
-    foreach (var claim in context.User.Claims)
-    {
-        a.Append(claim.ToString());
-    }
 
-    context.Response.WriteAsync(a.ToString());
-    return Task.CompletedTask;
-});
+#endregion
+
+#region App setup
+
 app.MapReverseProxy();
 app.UseSerilogRequestLogging();
 
 app.Start();
 
 IsHttpsEnabled(app);
+
+#endregion
 
 while (!app.Lifetime.ApplicationStopping.IsCancellationRequested) { }
 
