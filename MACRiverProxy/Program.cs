ec67e41cb0c7e151a1855c9d07461848f30fb84b
FantomLis <71078845+FantomLis@users.noreply.github.com>
2026-02-04 16:00:32 +0300

    Program wizard magic

--- MACRiverProxy/Program.cs
+++ MACRiverProxy/Program.cs
@@ -1,95 +1,115 @@
+using System.Net;
 using System.Security.Claims;
 using System.Text;
+using MACRiverProxy;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authorization.Infrastructure;
 using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-
-#region App builder setup
-
-builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));;
-builder.Services.AddSerilog(ser =>
+internal class Program
 {
-    ser.WriteTo.Console();
-});
-
-#endregion
-
-#region App builder auth setup
-
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
+    private static WebApplication app;
+    public static void Main(string[] args)
     {
-        options.LoginPath = "/login";
-        options.LogoutPath = "/logout";
-    });
+        var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("none", policyBuilder =>
-    {
-        policyBuilder.Requirements.Add(new AssertionRequirement(_ => true));
-    });
-    options.AddPolicy("restricted", policyBuilder =>
-    {
-        policyBuilder.RequireAuthenticatedUser();
-    });
-});
-
-#endregion
-
-var app = builder.Build();
-
-#region App auth setup
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-#endregion
-
-#region Login pages
-
-app.MapGet("/login", (context) => context.Request.HttpContext.
-    SignInAsync(new ClaimsPrincipal(new ClaimsIdentity([new Claim("test", "100")], CookieAuthenticationDefaults.AuthenticationScheme))));
-app.MapGet("/logout", (context) => context.Request.HttpContext.SignOutAsync());
-
-#endregion
-
-#region App setup
-
-app.MapReverseProxy();
-app.UseSerilogRequestLogging();
-
-app.Start();
-
-IsHttpsEnabled(app);
-
-#endregion
-
-while (!app.Lifetime.ApplicationStopping.IsCancellationRequested) { }
-
-void IsHttpsEnabled(WebApplication webApplication)
-{
-    bool isHttpsEnabled = false;
-
-    foreach (var url in webApplication.Urls)
-    {
-        if (url.ToLower().StartsWith("https://"))
+        #region App builder setup   
+        builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));;
+        builder.Services.AddSerilog(ser =>
         {
-            isHttpsEnabled = true;
-            break;
-        }
-    }
+            ser.WriteTo.Console();
+        });
 
-    if (!isHttpsEnabled)
-    {
-        Log.Error("=========================================");
-        Log.Error("HTTPS is not enabled on this proxy.");
-        Log.Error("This means, that all proxy traffic is transferred in plain-text."); 
-        Log.Error("Please enable HTTPS for traffic encryption.");
-        Log.Error("=========================================");
+        #endregion
+
+        #region App builder auth setup
+
+        builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options =>
+            {
+                options.LoginPath = "/login";
+                options.LogoutPath = "/logout";
+            });
+
+        builder.Services.AddAuthorization(options =>
+        {
+            options.AddPolicy("none", policyBuilder =>
+            {
+                policyBuilder.Requirements.Add(new AssertionRequirement(_ => true));
+            });
+            options.AddPolicy("restricted", policyBuilder =>
+            {
+                policyBuilder.RequireAuthenticatedUser();
+            });
+        });
+
+        #endregion
+
+        app = builder.Build();
+
+        #region App auth setup
+        app.MapStaticAssets().ShortCircuit();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        #endregion
+
+        #region Login pages
+
+        app.MapGet("/login", (context) => context.Request.HttpContext.
+            SignInAsync(new ClaimsPrincipal(new ClaimsIdentity([new Claim("test", "100")], CookieAuthenticationDefaults.AuthenticationScheme))));
+        app.MapGet("/logout", (context) =>
+        {
+            return context.Request.HttpContext.SignOutAsync();
+        });
+        app.MapGet("test", async context => 
+            context.Response.ThrowError(HttpStatusCode.Forbidden));
+
+        #endregion
+
+        #region App setup
+
+
+
+        app.MapReverseProxy();
+        app.UseSerilogRequestLogging();
+
+        app.Start();
+
+        IsHttpsEnabled(app);
+
+        #endregion
+
+        while (!app.Lifetime.ApplicationStopping.IsCancellationRequested) { }
+
+        void IsHttpsEnabled(WebApplication webApplication)
+        {
+            bool isHttpsEnabled = false;
+
+            foreach (var url in webApplication.Urls)
+            {
+                if (url.ToLower().StartsWith("https://"))
+                {
+                    isHttpsEnabled = true;
+                    break;
+                }
+            }
+
+            if (!isHttpsEnabled)
+            {
+                Log.Error("=========================================");
+                Log.Error("HTTPS is not enabled on this proxy.");
+                Log.Error("This means, that all proxy traffic is transferred in plain-text."); 
+                Log.Error("Please enable HTTPS for traffic encryption.");
+                Log.Error("=========================================");
+            }
+        }
+
+        
     }
+    
+    public static bool IsAppDevelopment () => app.Environment.IsDevelopment();
 }
