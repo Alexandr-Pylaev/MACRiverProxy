0547dfc25f3fe51fab8681faff477d8c78dc10d1
FantomLis <71078845+FantomLis@users.noreply.github.com>
2026-02-04 16:00:18 +0300

    Add Error pages

---
+++ MACRiverProxy/ErrorPage.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace MACRiverProxy;
+
+public static class ErrorPage
+{
+    const string ERR_GENERIC = "ERR_GENERIC";
+    public static string ThrowError(this HttpResponse response, HttpStatusCode code = HttpStatusCode.InternalServerError, 
+        string errorHeader = "Some error happened", string errorMessage = "Proxy thrown an error.\n" +
+                                                                           "No additional information provided.\n\n" +
+                                                                           "Contact administrator for additional help.",
+        string errorCode = ERR_GENERIC)
+    {
+        if (errorCode == ERR_GENERIC) Log.Error($"{ERR_GENERIC} was used. This error should be used only in development.");
+        bool isDone = false;
+        try
+        {
+            Log.Information($"Throwing error to client: {errorCode}");
+            response.StatusCode = (int)code;
+            response.WriteAsync(string.Format(File.ReadAllText("./Pages/ErrorPage.html"), errorHeader, errorMessage.Replace("\n", "<br/>"), errorCode));
+
+            isDone = true;
+        }
+        catch (FileNotFoundException e)
+        {
+            Log.Error($"Error page was not found. {e.Message}");
+            throw;
+        }
+        catch (Exception e)
+        {
+            Log.Error("Unexpected error when sending a error.");
+            Log.Error(e.ToString());
+        }
+        finally
+        {
+            if (!isDone)
+            {
+                response.StatusCode = ((int)HttpStatusCode.InternalServerError);
+            }
+        }
+
+        return "";
+    }
+}
