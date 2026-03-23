bd5d1e9e389f8827edaaee1b5c96336a6a74a1d4
FantomLis <71078845+FantomLis@users.noreply.github.com>
2026-03-23 20:05:24 +0300

    Move login to MACAuthentication

--- MACRiverProxy/Auth/MACAuthentication.cs
+++ MACRiverProxy/Auth/MACAuthentication.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace MACRiverProxy.Auth;
 
@@ -6,6 +8,15 @@ public class MACAuthentication
 {
     private static Lazy<MACAuthentication> _singleton = new();
     public static MACAuthentication Singleton => _singleton.Value;
+
+    public async Task SignIn(HttpContext context, TokenStorage tokenStorage, DateTime expires)
+    {
+        var token = new DebugTokenProvider().CreateToken();
+        tokenStorage.RegisterToken(expires, token);
+        await context.Request.HttpContext.SignInAsync(
+            new ClaimsPrincipal(
+                new ClaimsIdentity([token.AsClaim()], CookieAuthenticationDefaults.AuthenticationScheme)));
+    }
     public async Task SignOut(HttpContext context, TokenStorage tokenStorage)
     {
         tokenStorage.RevokeToken(context.User.FindFirst(Token.TOKEN_CLAIM_NAME).ToToken());
