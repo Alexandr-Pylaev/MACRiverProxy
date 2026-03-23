4a2e3d2b4f2330ea52c583dd0ac9b111da3a517b
Alexandr-Pylaev <236025710+Alexandr-Pylaev@users.noreply.github.com>
2026-03-23 18:29:08 +0300

    Add token expiring

--- MACRiverProxy/Auth/Token.cs
+++ MACRiverProxy/Auth/Token.cs
@@ -7,11 +7,13 @@ namespace MACRiverProxy.Auth;
 [JsonPolymorphic(
     UnknownDerivedTypeHandling = JsonUnknownDerivedTypeHandling.FallBackToNearestAncestor, TypeDiscriminatorPropertyName = "AuthMethod")]
 [JsonDerivedType(typeof(NullToken), "null")]
-public abstract partial class Token
+public abstract partial class Token(DateTime expireTime)
 {
     public byte MACLevel { get; set; } = 0;
     public ulong MACCategory { get; set; }= 0;
-    public byte[] AuthPayload { get; set; }
+    public byte[] AuthPayload { get; set; } = [];
+    public DateTime ExpireTime { get; init; } = expireTime;
+
     public void SetMACCategory(byte number, bool state)
     {
         if (number > 64) throw new ArgumentOutOfRangeException(nameof(number), "Maximum category number is 64.");
