a55f68f1ae12495e85af72bf6ebfe6c6b3813364
Alexandr-Pylaev <236025710+Alexandr-Pylaev@users.noreply.github.com>
2026-04-14 15:51:18 +0300

    Add password processing to LocalAuthUser

--- MACRiverProxy/Auth/LocalAuth/LocalAuthUser.cs
+++ MACRiverProxy/Auth/LocalAuth/LocalAuthUser.cs
@@ -2,5 +2,25 @@
 
 public class LocalAuthUser
 {
-    
+    private const string PASSWORD_PEPPER = "f9gfbnd98";
+    public static readonly int MAX_PASSWORD_LENGTH = 72 - (PASSWORD_PEPPER.Length);
+    public string Login { get; set; }
+    public string PasswordHash { get; protected set; }
+
+    public void SetPassword(string password)
+    {
+        if (password.Length > MAX_PASSWORD_LENGTH)
+            throw new ArgumentException($"Password is too long ({password.Length} > {MAX_PASSWORD_LENGTH})");
+        PasswordHash = BCrypt.Net.BCrypt.HashPassword(_PepperPassword(password));
+    }
+
+    private static string _PepperPassword(string password)
+    {
+        return PASSWORD_PEPPER + password;
+    }
+
+    public bool VerifyPassword(string password)
+    {
+        return BCrypt.Net.BCrypt.Verify(_PepperPassword(password), PasswordHash);
+    }
 }
