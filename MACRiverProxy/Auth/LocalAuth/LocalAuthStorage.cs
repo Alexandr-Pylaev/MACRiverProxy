79e73f6d39962f0161898d735f4734ec021842a9
FantomLis <71078845+FantomLis@users.noreply.github.com>
2026-04-14 15:43:27 +0300

    Create Local auth storage

---
+++ MACRiverProxy/Auth/LocalAuth/LocalAuthStorage.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MACRiverProxy.Auth.LocalAuth;
+
+public class LocalAuthStorage : DbContext
+{
+    public DbSet<LocalAuthUser> Users { get; set; }
+    public string DbPath { get; } = Path.Combine(Directory.GetCurrentDirectory(), "local-auth.db");
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
---
+++ MACRiverProxy/Auth/LocalAuth/LocalAuthUser.cs
@@ -0,0 +1,6 @@
+﻿namespace MACRiverProxy.Auth.LocalAuth;
+
+public class LocalAuthUser
+{
+    
+}
