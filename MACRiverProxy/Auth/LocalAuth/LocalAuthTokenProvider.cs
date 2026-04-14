6b2bfe51754053bc63ec7d4396eab38e121be1da
FantomLis <71078845+FantomLis@users.noreply.github.com>
2026-04-14 15:40:44 +0300

    Create Local Auth Provider class

---
+++ MACRiverProxy/Auth/LocalAuth/LocalAuthTokenProvider.cs
@@ -0,0 +1,16 @@
+﻿using MACRiverProxy.Auth.Tokens;
+
+namespace MACRiverProxy.Auth.LocalAuth;
+
+public class LocalAuthTokenProvider : TokenProvider
+{
+    public override Token CreateToken(params dynamic[]? args)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool VerifyToken(Token token)
+    {
+        throw new NotImplementedException();
+    }
+}
