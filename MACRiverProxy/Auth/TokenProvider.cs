99dbb505e547cfe6ac442c275afa1abec8385adf
FantomLis <71078845+FantomLis@users.noreply.github.com>
2026-04-14 14:04:49 +0300

    Add safer provider creation

--- MACRiverProxy/Auth/TokenProvider.cs
+++ MACRiverProxy/Auth/TokenProvider.cs
@@ -1,4 +1,6 @@
-﻿namespace MACRiverProxy.Auth;
+﻿using System.Reflection;
+
+namespace MACRiverProxy.Auth;
 
 public abstract class TokenProvider
 {
@@ -8,8 +10,25 @@ public abstract class TokenProvider
 
 public static class TokenProviderStatic
 {
-    public static TokenProvider CreateTokenProvider(this Token token)
+    public static TokenProvider CreateTokenProvider(this Token token, params object?[]? args)
     {
-        return Activator.CreateInstance(Type.GetType(token.AuthMethod + "TokenProvider")) as TokenProvider;
+        return Activator.CreateInstance(Type.GetType(token.AuthMethod + "TokenProvider"), args) as TokenProvider;
+    }
+
+    public static bool TryCreateTokenProvider(this Token token, out TokenProvider? provider, params object?[]? args)
+    {
+        provider = null;
+        try
+        {
+            provider = token.CreateTokenProvider(args);
+            return true;
+        }
+        catch (Exception ex) when (ex is ArgumentNullException or TargetInvocationException 
+                                       or TypeLoadException or ArgumentException 
+                                       or MethodAccessException or MemberAccessException 
+                                       or MissingMethodException)
+        {
+            return false;
+        }
     }
 }
