8110b31dad256009d58fa0e75f1250aa9d3d3088
Alexandr-Pylaev <236025710+Alexandr-Pylaev@users.noreply.github.com>
2026-04-27 20:12:58 +0300

    Add migration for LocalAuthStorage

---
+++ MACRiverProxy/Migrations/LocalAuthStorageMigrations/20260427171133_LocalAuthStorage added.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MACRiverProxy.Migrations.LocalAuthStorageMigrations
+{
+    /// <inheritdoc />
+    public partial class LocalAuthStorageadded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Login = table.Column<string>(type: "TEXT", nullable: false),
+                    MACLevel = table.Column<byte>(type: "INTEGER", nullable: false),
+                    MACCategory = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    PasswordHash = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Login);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
