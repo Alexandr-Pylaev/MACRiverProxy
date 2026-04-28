c4c4938b7f13adbf499da9af943ccc89558bd36d
FantomLis <71078845+FantomLis@users.noreply.github.com>
2026-04-28 20:40:47 +0300

    😭😭😭

--- MACRiverProxy/Auth/Tokens/NullTokenProvider.cs
+++ MACRiverProxy/Auth/Tokens/NullTokenProvider.cs
@@ -6,8 +6,7 @@
 public class NullTokenProvider : TokenProvider
 {
     // ReSharper disable once StaticMemberInitializerReferesToMemberBelow
-    public static NullTokenProvider Singleton = _singleton!.Value;
-    private static Lazy<NullTokenProvider> _singleton = new();
+    public static NullTokenProvider Singleton = new ();
     public override Token CreateToken(params dynamic[]? args) => new Token(this.TokenProviderName);
 
     public override bool VerifyToken(Token token)
