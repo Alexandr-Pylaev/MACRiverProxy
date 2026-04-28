2e146f4c8e09402766c1883a34f9a291f50faa32
FantomLis <71078845+FantomLis@users.noreply.github.com>
2026-04-28 21:35:43 +0300

    Fix some login stuff

--- MACRiverProxy/Auth/MACAuthentication.cs
+++ MACRiverProxy/Auth/MACAuthentication.cs
@@ -11,18 +11,19 @@ public class MACAuthentication
     private static Lazy<MACAuthentication> _singleton = new();
     public static MACAuthentication Singleton => _singleton.Value;
 
-    public async Task SignIn(HttpContext context, TokenProvider provider, TokenStorage tokenStorage, DateTime expires, params dynamic[]? args)
+    public async Task<bool> SignIn(HttpContext context, TokenProvider provider, TokenStorage tokenStorage, DateTime expires, params dynamic[]? args)
     {
         var token = provider.CreateToken(args);
         if (token == TokenProvider.Empty)
         {
             context.Response.Redirect("/login?error=Failed%20to%20verify%20info%20you%20provided.");
-            return;
+            return false;
         }
         tokenStorage.RegisterToken(expires, token);
         await context.Request.HttpContext.SignInAsync(
             new ClaimsPrincipal(
                 new ClaimsIdentity([token.AsClaim()], CookieAuthenticationDefaults.AuthenticationScheme)));
+        return true;
     }
     public async Task SignOut(HttpContext context, TokenStorage tokenStorage, TokenProvider provider)
     {
--- MACRiverProxy/Auth/Tokens/TokenProvider.cs
+++ MACRiverProxy/Auth/Tokens/TokenProvider.cs
@@ -7,7 +7,8 @@ public abstract class TokenProvider
     public abstract Token CreateToken(params dynamic[]? args);
     public abstract bool VerifyToken(Token token);
     public abstract bool DestroyToken(Token token);
-    public static Token Empty => NullTokenProvider.Singleton.CreateToken();
+    public static Token Empty => _empty;
+    private static Token _empty = NullTokenProvider.Singleton.CreateToken();
     protected string TokenProviderName 
     {
         get
