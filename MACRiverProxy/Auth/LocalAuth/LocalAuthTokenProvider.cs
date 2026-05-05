c098901992c78b5e4d7d8e576f777b1adca6a2fe
Alexandr-Pylaev <236025710+Alexandr-Pylaev@users.noreply.github.com>
2026-05-05 17:40:38 +0300

    Revert "Fix missing db files"
    This reverts commit 96c1956c52e3ad20f00ac616cd8a78ecb33415e3.

--- MACRiverProxy/Auth/LocalAuth/LocalAuthTokenProvider.cs
+++ MACRiverProxy/Auth/LocalAuth/LocalAuthTokenProvider.cs
@@ -51,7 +51,6 @@ public static class LocalAuthTokenProviderStatic
         col.TryAddSingleton(tokenStorage);
         col.TryAddSingleton(localAuthStorage);
         col.TryAddSingleton(authTokenProvider);
-        localAuthStorage.Database.EnsureCreated();
         localAuthStorage.Database.Migrate();
     }
 }
