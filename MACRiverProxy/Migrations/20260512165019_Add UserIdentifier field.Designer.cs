e83d2dd0014f8ca5dd94e329c2ea1ece60c55ad7
Alexandr-Pylaev <236025710+Alexandr-Pylaev@users.noreply.github.com>
2026-05-12 19:52:39 +0300

    Add migration for TokenKeyStorage

--- MACRiverProxy/Migrations/20260512165019_Add UserIdentifier field.Designer.cs
+++ MACRiverProxy/Migrations/20260512165019_Add UserIdentifier field.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MACRiverProxy.Auth.Tokens;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MACRiverProxy.Migrations
 {
     [DbContext(typeof(TokenKeyStorage))]
-    partial class TokenKeyStorageModelSnapshot : ModelSnapshot
+    [Migration("20260512165019_Add UserIdentifier field")]
+    partial class AddUserIdentifierfield
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.15");
@@ -25,7 +28,7 @@ namespace MACRiverProxy.Migrations
 
                     b.Property<DateTime>("ExpireTime")
                         .HasColumnType("TEXT");
-                    
+
                     b.Property<string>("UserIdentifier")
                         .IsRequired()
                         .HasColumnType("TEXT");
