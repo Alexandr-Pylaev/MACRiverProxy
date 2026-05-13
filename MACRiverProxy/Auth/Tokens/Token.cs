29af75354109461d1c65c01f67a9d96abda84ebd
Alexandr-Pylaev <236025710+Alexandr-Pylaev@users.noreply.github.com>
2026-05-13 15:50:50 +0300

    Add better docs for Token file

--- MACRiverProxy/Auth/Tokens/Token.cs
+++ MACRiverProxy/Auth/Tokens/Token.cs
@@ -27,11 +27,16 @@ public sealed class Token(string authMethod, string userIdentifier) : IMACTag
     /// <remarks>Always set this field in <see cref="TokenProvider"/> because this field is used for tracking user activity</remarks>
     public string UserIdentifier { get; set; } = userIdentifier;
 
+    /// <summary>
+    /// Token key object.
+    /// </summary>
+    /// <remarks>Can be null when <see cref="TokenKey"/> is not same as in <see cref="TokenKeyEntry"/></remarks>
     [JsonIgnore]
     public TokenKey? TokenKeyEntry
     {
         get
         {
+            // Return TokenKey object only if keys are equal
             if (_tokenKey?.Key != TokenKey)
             {
                 _tokenKey = null;
@@ -41,6 +46,7 @@ public sealed class Token(string authMethod, string userIdentifier) : IMACTag
         }
         set
         {
+            // Sets token key values to token
             if (value is null) return;
             TokenKey = value.Key;
             ExpireTime = value.ExpireTime;
@@ -51,27 +57,44 @@ public sealed class Token(string authMethod, string userIdentifier) : IMACTag
 
     private TokenKey? _tokenKey;
 
+    /// <summary>
+    /// Converts token to a <see cref="Claim"/>
+    /// </summary>
+    /// <returns>Claim with <see cref="TOKEN_CLAIM_NAME"/> type and token's JSON as value</returns>
     public Claim AsClaim()
     {
         return new Claim(TOKEN_CLAIM_NAME, JsonSerializer.Serialize(this));
     }
 
+    /// <summary>
+    /// Token claim type name
+    /// </summary>
     public const string TOKEN_CLAIM_NAME = "Token";
 }
 
 public static class TokenStatic
 {
+    /// <summary>
+    /// Gets token from <see cref="HttpContext.User"/> claims
+    /// </summary>
+    /// <returns>Token if claim exists or null if not</returns>
     public static Token? GetUserToken(this HttpContext context)
     {
         return context.User
             .FindFirst(Token.TOKEN_CLAIM_NAME)?
             .ToToken();
     }
+    /// <summary>
+    /// Converts claim to token.
+    /// </summary>
+    /// <param name="claim">Claim with type <see cref="Token.TOKEN_CLAIM_NAME"/></param>
+    /// <returns>Deserialized token from claim value</returns>
+    /// <remarks>Returns <see cref="TokenProvider.Empty"/> when claim is null or cannot be deserialized.</remarks>
     public static Token ToToken(this Claim? claim)
     {
         try
         {
-            return JsonSerializer.Deserialize<Token>(claim?.Value ?? JsonSerializer.Serialize(TokenProvider.Empty),
+            return JsonSerializer.Deserialize<Token>(claim?.Value ?? JsonSerializer.Serialize(TokenProvider.Empty), // Serializes empty if claim is null
                 new JsonSerializerOptions()
                 {
                     AllowOutOfOrderMetadataProperties = true
@@ -83,6 +106,9 @@ public static class TokenStatic
         }
     }
     
+    /// <summary>
+    /// Verifies token against specified <see cref="TokenKeyStorage"/> and <see cref="TokenProvider"/>
+    /// </summary>
     public static async Task<bool> VerifyToken(this Token? token, TokenKeyStorage? tokenStorage, TokenProvider? tokenProvider) =>
         (await (tokenStorage?.CheckToken(token) ?? Task.FromResult(false))) 
         && (tokenProvider?.VerifyToken(token!) ?? false);
