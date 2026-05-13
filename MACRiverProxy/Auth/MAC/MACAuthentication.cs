e18129b4430ab5bb91afb617a721be442c32dc03
Alexandr-Pylaev <236025710+Alexandr-Pylaev@users.noreply.github.com>
2026-05-13 17:38:16 +0300

    Add docs for MACAuthenticationStatic

--- MACRiverProxy/Auth/MAC/MACAuthentication.cs
+++ MACRiverProxy/Auth/MAC/MACAuthentication.cs
@@ -6,8 +6,20 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 namespace MACRiverProxy.Auth.MAC;
 
 // ReSharper disable once InconsistentNaming
+/// <summary>
+/// Authentication extensions for mandatory access control
+/// </summary>
 public static class MACAuthenticationStatic
 {
+    
+    /// <summary>
+    /// Sign in user, if data is valid.
+    /// </summary>
+    /// <param name="context">Request context</param>
+    /// <param name="provider">Token provider, that supports form auth</param>
+    /// <param name="expires">Expiration date</param>
+    /// <returns>Is user sign in</returns>
+    /// <exception cref="InvalidOperationException">Context does not have TokenStorage service.</exception>
     public static async Task<bool> SignIn(this HttpContext context, FormTokenProvider provider, DateTime expires)
     {
         var token = provider.CreateTokenFromForm(context.Request.Form);
@@ -26,6 +38,40 @@ public static class MACAuthenticationStatic
                 new ClaimsIdentity([token.AsClaim()], CookieAuthenticationDefaults.AuthenticationScheme)));
         return true;
     }
+
+    /// <summary>
+    /// Sign in user, if data is valid.
+    /// </summary>
+    /// <param name="context">Request context</param>
+    /// <param name="provider">Token provider</param>
+    /// <param name="expires">Expiration date</param>
+    /// <param name="args">Args for auth</param>
+    /// <returns>Is user sign in</returns>
+    /// <exception cref="InvalidOperationException">Context does not have TokenStorage service.</exception>
+    public static async Task<bool> SignIn(this HttpContext context, TokenProvider provider, DateTime expires, params string?[]? args)
+    {
+        var token = provider.CreateToken(args);
+        var tokenStorage = context.RequestServices.GetService<TokenKeyStorage>();
+        if (tokenStorage is null)
+        {
+            throw new InvalidOperationException("Context does not have TokenStorage service.");
+        }
+        if (token == TokenProvider.Empty)
+        {
+            return false;
+        }
+        await tokenStorage.RegisterToken(expires, token);
+        await context.SignInAsync(
+            new ClaimsPrincipal(
+                new ClaimsIdentity([token.AsClaim()], CookieAuthenticationDefaults.AuthenticationScheme)));
+        return true;
+    }
+    /// <summary>
+    /// Sign out user
+    /// </summary>
+    /// <param name="context">Request context</param>
+    /// <returns>Is user successfully sign out</returns>
+    /// <remarks>If token is not set up, always returns true.</remarks>
     public static async Task<bool> SignOut(this HttpContext context)
     {
         var token = context.GetUserToken();
