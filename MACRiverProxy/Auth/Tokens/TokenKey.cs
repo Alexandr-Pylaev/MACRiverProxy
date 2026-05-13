2b676effa3eeec65e3b8e0ae4ad9930d3be698fc
Alexandr-Pylaev <236025710+Alexandr-Pylaev@users.noreply.github.com>
2026-05-13 16:00:44 +0300

    Add docs for TokenKey

--- MACRiverProxy/Auth/Tokens/TokenKey.cs
+++ MACRiverProxy/Auth/Tokens/TokenKey.cs
@@ -3,11 +3,17 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MACRiverProxy.Auth.Tokens;
 
+/// <summary>
+/// Database model for storing token key and associated values
+/// </summary>
 public class TokenKey
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public string Key { get; init; }
     public DateTime ExpireTime { get; set; }
+    /// <summary>
+    /// Text, that identifies user and allow user tracking
+    /// </summary>
     public string UserIdentifier { get; set; }
 }
