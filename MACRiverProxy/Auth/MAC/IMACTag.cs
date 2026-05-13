cd38fb20eb9255b57a267d7b1470d6be478c76b7
Alexandr-Pylaev <236025710+Alexandr-Pylaev@users.noreply.github.com>
2026-05-13 15:36:39 +0300

    Rename IMACTag

--- MACRiverProxy/Auth/MAC/IMACTag.cs
+++ MACRiverProxy/Auth/MAC/IMACTag.cs
@@ -1,15 +1,19 @@
 ﻿namespace MACRiverProxy.Auth.MAC;
 
 // ReSharper disable InconsistentNaming
-public interface IMAC
+/// <summary>
+/// Interface for objects, that uses mandatory access control tag
+/// </summary>
+public interface IMACTag
 {
+    
     public byte MACLevel { get; set; }
     public ulong MACCategory { get; set; }
 }
 
 public static class MAC
 {
-    public static void SetMACCategory(this IMAC mac, byte number, bool state)
+    public static void SetMACCategory(this IMACTag mac, byte number, bool state)
     {
         if (number > 64) throw new ArgumentOutOfRangeException(nameof(number), "Maximum category number is 64.");
 
@@ -23,30 +27,30 @@ public static class MAC
         }
     }
 
-    public static bool HaveLevel(this IMAC mac, byte level) => mac.MACLevel >= level;
+    public static bool HaveLevel(this IMACTag mac, byte level) => mac.MACLevel >= level;
 
-    public static bool HaveCategories(this IMAC mac, ulong category)
+    public static bool HaveCategories(this IMACTag mac, ulong category)
     {
         if (category == 0) return true;
         return (mac.MACCategory & category) != 0;
     }
 
-    public static bool HaveCategory(this IMAC mac, byte number)
+    public static bool HaveCategory(this IMACTag mac, byte number)
     {
         if (number == 0) return true;
         if (number > 64) throw new ArgumentOutOfRangeException(nameof(number), "Maximum category number is 64.");
         return mac.HaveCategories((1ul << number));
     }
 
-    public static void AddMACCategory(this IMAC mac,byte number) => mac.SetMACCategory(number, true);
-    public static void RemoveMACCategory(this IMAC mac,byte number) => mac.SetMACCategory(number, false);
+    public static void AddMACCategory(this IMACTag mac,byte number) => mac.SetMACCategory(number, true);
+    public static void RemoveMACCategory(this IMACTag mac,byte number) => mac.SetMACCategory(number, false);
 
-    public static void AddMACLevel(this IMAC mac,byte level)
+    public static void AddMACLevel(this IMACTag mac,byte level)
     {
         if (mac.MACLevel < level) mac.MACLevel = level;
     }
 
-    public static void RemoveMACLevel(this IMAC mac,byte level)
+    public static void RemoveMACLevel(this IMACTag mac,byte level)
     {
         if (mac.MACLevel > level) mac.MACLevel = level;
     }
