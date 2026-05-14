5144dc597307b342c006ea22701b916fa2900072
Alexandr-Pylaev <236025710+Alexandr-Pylaev@users.noreply.github.com>
2026-05-14 17:57:13 +0300

    Add migration for ActiveTokenKeys renaming

---
+++ MACRiverProxy/Migrations/20260514142559_Rename ActiveTokenKeys.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MACRiverProxy.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameActiveTokenKeys : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ActiveTokens",
+                table: "ActiveTokens");
+
+            migrationBuilder.RenameTable(
+                name: "ActiveTokens",
+                newName: "ActiveTokenKeys");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ActiveTokenKeys",
+                table: "ActiveTokenKeys",
+                column: "Key");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ActiveTokenKeys",
+                table: "ActiveTokenKeys");
+
+            migrationBuilder.RenameTable(
+                name: "ActiveTokenKeys",
+                newName: "ActiveTokens");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ActiveTokens",
+                table: "ActiveTokens",
+                column: "Key");
+        }
+    }
+}
