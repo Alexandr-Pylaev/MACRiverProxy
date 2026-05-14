f689d8362183531cad925214adc506bac5815a8d
Alexandr-Pylaev <236025710+Alexandr-Pylaev@users.noreply.github.com>
2026-05-14 13:42:55 +0300

    Add docs for RouteStatic

--- MACRiverProxy/RouteStatic.cs
+++ MACRiverProxy/RouteStatic.cs
@@ -1,12 +1,22 @@
 ﻿using MACRiverProxy.Auth.MAC;
 using MACRiverProxy.Auth.Tokens;
 using Serilog;
+using Yarp.ReverseProxy.Configuration;
 
 namespace MACRiverProxy;
-
+/// <summary>
+/// Extension class for YARP proxy routing
+/// </summary>
 public static class RouteStatic
 {
-    public static async Task<bool> AuthorizeTokenForRoute(this Token? token, IServiceProvider sp, string routeId)
+    /// <summary>
+    /// <inheritdoc cref="AuthorizeTokenForRoute(Token, string, IConfiguration, TokenKeyStorage, TokenProvider)"/>
+    /// </summary>
+    /// <param name="token">Token</param>
+    /// <param name="routeId">ID of route from config</param>
+    /// <param name="sp">Service provider</param>
+    /// <returns><inheritdoc cref="AuthorizeTokenForRoute(Token, string, IConfiguration, TokenKeyStorage, TokenProvider)"/></returns>
+    public static async Task<bool> AuthorizeTokenForRoute(this Token? token, string routeId, IServiceProvider sp)
     {
         if (token is null) return false;
         
@@ -18,6 +28,16 @@ public static class RouteStatic
         
         return await AuthorizeTokenForRoute(token, routeId, configServ, tokenStorageServ, tokenProvider);
     }
+
+    /// <summary>
+    /// Authorize token against route with <see cref="Restricted"/> <see cref="RouteConfig.AuthorizationPolicy"/>
+    /// </summary>
+    /// <param name="token">Token</param>
+    /// <param name="routeId">ID of route from config</param>
+    /// <param name="config">Configuration of proxy</param>
+    /// <param name="tokenStorage">Token storage</param>
+    /// <param name="tokenProvider">Token provider</param>
+    /// <returns>Is token authorized for this route</returns>
     public static async Task<bool> AuthorizeTokenForRoute(Token token, string routeId, IConfiguration config, TokenKeyStorage tokenStorage,
         TokenProvider tokenProvider)
     {
@@ -57,8 +77,16 @@ public static class RouteStatic
 
         return true;
     }
+
     public const string Restricted = "restricted";
-    
+    /// <summary>
+    /// Gets config value from ReverseProxy:Routes config path
+    /// </summary>
+    /// <param name="configServ">Configuration</param>
+    /// <param name="routeId">ID of route from config</param>
+    /// <param name="key">Value key</param>
+    /// <returns><typeparamref name="T"/> from config or null, when value is not found.</returns>
+    /// <exception cref="InvalidOperationException">Throws when value cannot be converted to target type.</exception>
     public static T? GetRouteConfigValue<T>(this IConfiguration configServ, string routeId, string key)
     {
         return configServ.GetValue<T?>($"ReverseProxy:Routes:{routeId}:{key}");
