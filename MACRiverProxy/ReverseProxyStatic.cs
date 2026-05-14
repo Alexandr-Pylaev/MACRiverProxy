9a25f385bd74f0d0fc11538f6625d01c4bfcb64c
Alexandr-Pylaev <236025710+Alexandr-Pylaev@users.noreply.github.com>
2026-05-14 14:17:36 +0300

    Add docs for ReverseProxyStatic

--- MACRiverProxy/ReverseProxyStatic.cs
+++ MACRiverProxy/ReverseProxyStatic.cs
@@ -3,9 +3,16 @@ using Serilog;
 using Yarp.ReverseProxy.Forwarder;
 
 namespace MACRiverProxy;
-
+/// <summary>
+/// Extension class for YARP proxying
+/// </summary>
 public static class ReverseProxyStatic
 {
+    /// <summary>
+    /// Gets return URL from request URL.
+    /// </summary>
+    /// <param name="request">Request</param>
+    /// <returns>Value of ReturnURL query</returns>
     public static string GetRedirectUrl(this HttpRequest request)
     {
         if (request.Query.TryGetValue("ReturnURL", out var returnUrl))
@@ -15,22 +22,31 @@ public static class ReverseProxyStatic
 
         return "/";
     }
+    /// <summary>
+    /// Redirects <see cref="HttpContext.Response"/> to ReturnURL value.
+    /// </summary>
+    /// <param name="context">Http context</param>
     public static void RedirectToRedirectUrl(this HttpContext context)
     {
         context.Response.Redirect(context.Request.GetRedirectUrl());
     }
+    /// <summary>
+    /// Adds failed proxying error display middleware.
+    /// </summary>
+    /// <param name="proxyOpt">Proxy options</param>
+    /// <returns><paramref name="proxyOpt"/></returns>
     public static IReverseProxyApplicationBuilder UseProxyingErrorDisplayMiddleware(this IReverseProxyApplicationBuilder proxyOpt)
     {
         proxyOpt.Use(async (context, next) =>
         {
             // Awaiting when other middlewares finishes
             await next();
-
+            // Gets YARP ForwarderErrorFeature, that contains proxying error
             var errorFeature = context.GetForwarderErrorFeature();
             if (errorFeature?.Exception == null) return;
             string header, msg;
             switch (errorFeature.Error)
-            {
+            {   // Resource is not responding
                 case ForwarderError.RequestTimedOut:
                 case ForwarderError.Request:
                     header = "Target resource is not responding.";
@@ -40,13 +56,14 @@ public static class ReverseProxyStatic
                     header = "Resource is not available.";
                     msg = "Resource you trying to access is not available.";
                     break;
+                // Default response
                 default:
                     header = "Failed to connect to target resource.";
                     msg =
                         "While trying to connect to target resource, error happened.\nUse error code for more info.";
                     break;
             }
-
+            // Send user error page with proxying error
             await context.SendErrorPageAsync(HttpStatusCode.BadGateway,
                 header,
                 msg,
@@ -55,7 +72,11 @@ public static class ReverseProxyStatic
         });
         return proxyOpt;
     }
-    
+    /// <summary>
+    /// Escapes spaces with %20
+    /// </summary>
+    /// <param name="text">Original text</param>
+    /// <returns>Escaped text</returns>
     public static string EscapeCharactersForUrl(this string text)
     {
         return text.Replace(" ", "%20");
