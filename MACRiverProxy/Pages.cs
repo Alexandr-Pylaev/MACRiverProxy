6117ae3604c74162532d538e120bed89e173ea19
Alexandr-Pylaev <236025710+Alexandr-Pylaev@users.noreply.github.com>
2026-05-14 16:17:28 +0300

    Fix SendStoredPageAsync error has wrong text

--- MACRiverProxy/Pages.cs
+++ MACRiverProxy/Pages.cs
@@ -14,7 +14,7 @@ public static class Pages
         }
         catch (FileNotFoundException e)
         {
-            Log.Error("Login page was not found. {eMsg}", e.Message);
+            Log.Error("Page was not found. {eMsg}", e.Message);
             if (Program.IsAppDevelopment()) Log.Error(e.ToString());
             throw;
         }
