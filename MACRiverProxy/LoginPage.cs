62a3ac8d38f3d03048d49a4d98726d71ada20b4d
Alexandr-Pylaev <236025710+Alexandr-Pylaev@users.noreply.github.com>
2026-05-18 20:54:11 +0300

    Fix & adds every time RedirectWithLoginError

--- MACRiverProxy/LoginPage.cs
+++ MACRiverProxy/LoginPage.cs
@@ -27,7 +27,9 @@ public static class LoginPage
     /// <param name="response">HTTP response</param>
     /// <param name="error">Error text</param>
     public static void RedirectWithLoginError(this HttpResponse response, string error) {
-        response.Redirect($"/login?error={error.EscapeCharactersForUrl()}&{Regex.Replace(response.HttpContext.Request.QueryString.Value?.Replace("?", "") 
-            ?? "", @"&?error=[^&]*", "")}");
+        response.Redirect($"/login?error={error.EscapeCharactersForUrl()}" +
+                          $"&{Regex.Replace(response.HttpContext.Request.QueryString.Value?
+                                                .Replace("?", "") 
+                                            ?? "", @"&?error=[^&]*", "").TrimStart().TrimStart('&')}");
     }
 }
